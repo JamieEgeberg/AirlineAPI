2365a27d2d8a6e99eff85636d91cfc865cc4e516
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-05-10 11:16:52 +0200

    Reservation response entity elaboration

--- src/main/java/entity/ReservationResponse.java
+++ src/main/java/entity/ReservationResponse.java
@@ -5,6 +5,10 @@
  */
 package entity;
 
+import com.google.gson.annotations.Expose;
+import java.util.List;
+import javax.persistence.OneToMany;
+
 /**
  *
  * @author Jamie
@@ -13,7 +17,39 @@ public class ReservationResponse {
 
     public ReservationResponse() {
     }
-    
-    
-    
+
+    public ReservationResponse(Flight flight, Reservation res) {
+        flightNumber = flight.getFlightNumber();
+        origin = flight.getOrigin();
+        destination = flight.getDestination();
+        date = flight.getDate();
+        numberOfSeats = res.getNumberOfSeats();
+        flightTime = flight.getTraveltime();
+        reserveeName = res.getReserveeName();
+        passengers = res.getPassengers();
+    }
+
+    @Expose
+    String flightNumber;
+
+    @Expose
+    String origin; // IATA-Code
+
+    @Expose
+    String destination; // IATA-Code
+
+    @Expose
+    String date; // ISO-8601 date+time
+
+    @Expose
+    int numberOfSeats;
+
+    @Expose
+    int flightTime;
+
+    String reserveeName;
+
+    @OneToMany
+    List<Passenger> passengers;
+
 }
