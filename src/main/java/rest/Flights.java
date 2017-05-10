de1763e467d69998611d35d836e179f1247ff2b4
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-05-10 15:16:13 +0200

    enabled usage of Database

--- src/main/java/rest/Flights.java
+++ src/main/java/rest/Flights.java
@@ -2,8 +2,12 @@ package rest;
 
 import com.google.gson.Gson;
 import entity.Airline;
+import entity.Flight;
+import facades.FlightFacade;
 import utils.Generator;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,6 +15,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import java.util.List;
 
 /**
  * Created by Niki on 2017-05-01.
@@ -20,10 +25,18 @@ import javax.ws.rs.core.UriInfo;
 @Path("flights")
 public class Flights {
 
+    private EntityManagerFactory emf;
+    private FlightFacade facade;
+
     private static Gson gson = new Gson();
 
     @Context
     private UriInfo context;
+
+    public Flights() {
+        emf = Persistence.createEntityManagerFactory("pu_development");
+        facade = new FlightFacade(emf);
+    }
 
     @GET
     @Path("/{from}/{date}/{tickets}")
@@ -31,8 +44,22 @@ public class Flights {
     public String getFlightsFromDateTickets(@PathParam("from") String from,
                                             @PathParam("date") String date,
                                             @PathParam("tickets") int tickets) {
-        Airline airline = Generator.generateFlights(from, date, tickets);
-        return gson.toJson(airline);
+
+        List<Flight> flights = facade.getFlightsByOriginAndDate(from, date);
+        if (flights != null && flights.size() > 0) {
+            for (int i = 0; i < flights.size(); i++)
+                flights.get(i).setNumberOfSeats(tickets);
+            Airline airline = new Airline();
+            airline.airline = "JENS air";
+            airline.flights = flights;
+            return gson.toJson(airline);
+        } else {
+            Airline airline = Generator.generateFlights(from, date, tickets);
+            for (int i = 0; i < airline.flights.size(); i++) {
+                facade.addFlight(airline.flights.get(i));
+            }
+            return gson.toJson(airline);
+        }
     }
 
     @GET
@@ -42,8 +69,25 @@ public class Flights {
                                             @PathParam("to") String to,
                                             @PathParam("date") String date,
                                             @PathParam("tickets") int tickets) {
-        Airline airline = Generator.generateFlights(from, to, date, tickets);
-        return gson.toJson(airline);
+        List<Flight> flights = facade.getFlightsByOriginAndDestinationAndDate
+                (from, to, date);
+        if (flights != null && flights.size() > 0) {
+            for (int i = 0; i < flights.size(); i++)
+                flights.get(i).setNumberOfSeats(tickets);
+            Airline airline = new Airline();
+            airline.airline = "JENS air";
+            airline.flights = flights;
+            return gson.toJson(airline);
+        } else {
+            Airline airline = Generator.generateFlights(from,
+                                                        to,
+                                                        date,
+                                                        tickets);
+            for (int i = 0; i < airline.flights.size(); i++) {
+                facade.addFlight(airline.flights.get(i));
+            }
+            return gson.toJson(airline);
+        }
     }
 
 }
