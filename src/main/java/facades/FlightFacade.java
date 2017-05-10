41e7e26946135af3c99d677f48150523195d9f21
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-05-10 11:01:56 +0200

    adding functionality to the flight facade

--- src/main/java/facades/FlightFacade.java
+++ src/main/java/facades/FlightFacade.java
@@ -6,15 +6,12 @@
 package facades;
 
 import entity.Flight;
-import entity.User;
+import entity.Reservation;
+import entity.ReservationResponse;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
-import security.IUser;
-import security.PasswordStorage;
 
 /**
  *
@@ -32,10 +29,10 @@ public class FlightFacade {
         return emf.createEntityManager();
     }
 
-    public IUser getFlightByFlightId(String id) {
+    public Flight getFlightByFlightId(String id) {
         EntityManager em = getEntityManager();
         try {
-            return em.find(User.class, id);
+            return em.find(Flight.class, id);
         } finally {
             em.close();
         }
@@ -43,7 +40,7 @@ public class FlightFacade {
 
     public List<Flight> getFlightsByOriginAndDate(String origin, String date) {
         EntityManager em = getEntityManager();
-        TypedQuery<Flight> query = em.createQuery("SELECT f FROM FLIGHT f WHERE f.origin=:org AND f.date LIKE :date",Flight.class);
+        TypedQuery<Flight> query = em.createQuery("SELECT f FROM FLIGHT f WHERE f.origin=:org AND f.date LIKE :date", Flight.class);
         query.setParameter("org", origin);
         query.setParameter("date", date.toLowerCase().split("t")[0]);
         return query.getResultList();
@@ -52,35 +49,34 @@ public class FlightFacade {
     /*
   Return the Roles if users could be authenticated, otherwise null
      */
-    public List<String> authenticateUser(String userName, String password) {
-        IUser user = getFlightByFlightId(userName);
-        if (user == null) {
-            return null;
-        }
-        boolean passwordOK = false;
-        try {
-            passwordOK = PasswordStorage.verifyPassword(password, user.getPassword());
-        } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
-            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return user != null && passwordOK ? user.getRolesAsStrings() : null;
-    }
-
-    public IUser addUser(String userName, String password) {
+    public Flight addFlight(Flight flight) {
         EntityManager em = getEntityManager();
         try {
-            User user = new User(userName, password);
             em.getTransaction().begin();
-            em.persist(user);
+            em.persist(flight);
             em.getTransaction().commit();
-            return user;
+            return flight;
 
-        } catch (PasswordStorage.CannotPerformOperationException ex) {
-            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
         } finally {
             em.close();
         }
     }
 
+    public Reservation addReservation(Reservation reservation) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(reservation);
+            em.getTransaction().commit();
+            return reservation;
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public ReservationResponse getReservationResponse(Reservation res) {
+        Flight flight = getFlightByFlightId(res.getFlightID());
+        return new ReservationResponse(flight, res);
+    }
 }
