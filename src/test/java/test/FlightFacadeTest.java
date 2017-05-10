065f82d620d1209d15a450891c607f8b945566fe
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-05-10 11:00:15 +0200

    flight facade test started

--- src/test/java/test/FlightFacadeTest.java
+++ src/test/java/test/FlightFacadeTest.java
@@ -1,76 +1,51 @@
 package test;
 
+import entity.Flight;
 import entity.PU;
-import facades.UserFacade;
+import facades.FlightFacade;
 import java.util.List;
 import javax.persistence.Persistence;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import security.IUser;
-import security.IUserFacade;
+import utils.Generator;
 
-public class UserFacadeTest {
-  
-  IUserFacade facade ;
-  
-  //Override this in a derived class to use an alternative database
-  public void setPersistenceUnit() {
-    PU.setPU_Name("pu_memorydb_mock");
-  }
-  
-  @Before
-  public void initFacadeAndTestUsers(){
-    setPersistenceUnit(); 
-    facade = new UserFacade(Persistence.createEntityManagerFactory(PU.getPersistenceUnitName()));
-    //Setup test users
-    utils.makeTestUsers.main(null);
-  }
-  
-  @Test
-  public void testGetExsistingUserById() {
-    IUser user = facade.getUserByUserId("user");
-    System.out.println(user.getPassword());
-    assertEquals("user", user.getUserName());
-  }
-  
-  @Test
-  public void testGetNonExsistingUserById() {
-    IUser user = facade.getUserByUserId("i_dont_exist");
-    assertNull(user);
-  }
+public class FlightFacadeTest {
 
-  @Test
-  public void testAuthenticateUserOK() {
-    List<String> result = facade.authenticateUser("user", "test");
-    assertEquals("User should have one role",1, result.size());
-  }
-  
-  @Test
-  public void testAuthenticateUserWrongPassword() {
-    List<String> result = facade.authenticateUser("user", "wrong password");
-    assertNull("Should be null, if user could not login",result);
-  }
-  
-  @Test
-  public void testAuthenticateUserDoesNotExist() {
-    List<String> result = facade.authenticateUser("iDontExist", "password");
-    assertNull("Should be null, if user does not exists",result);
-  }
-  
-  @Test
-  public void passwordIsHashedForNewUsers() {
-    IUser user = facade.addUser("Kurt", "Secret");
-    assertEquals("Kurt", user.getUserName());
-    assertTrue("Password should be hashed",user.getPassword().startsWith("sha1:64000:"));
-  }
-  
-  @Test
-  public void addUser() {
-    facade.addUser("Kurt", "Secret");   
-    //Verify that user was actually inserted in the database
-    IUser newUser = facade.getUserByUserId("Kurt");
-    assertEquals("Kurt", newUser.getUserName());
-  }
-  
+    FlightFacade facade;
+    Flight testFlight;
+
+    //Override this in a derived class to use an alternative database
+    public void setPersistenceUnit() {
+        PU.setPU_Name("pu_memorydb_mock");
+    }
+
+    @Before
+    public void initFacadeAndTestUsers() {
+        setPersistenceUnit();
+        facade = new FlightFacade(Persistence.createEntityManagerFactory(PU.getPersistenceUnitName()));
+        //Setup test users
+        utils.makeTestUsers.main(null);
+    }
+
+    @Test
+    public void testGetFlightByFlightId() {
+        testFlight = Generator.generateFlights("CPH", "CDG", 1).flights.get(0);
+        Flight flight = facade.getFlightByFlightId(testFlight.getFlightID());
+        assertEquals(testFlight.getFlightID(), flight.getFlightID());
+        assertEquals(testFlight.getFlightNumber(), flight.getFlightNumber());
+    }
+
+    @Test
+    public void testAddFlight() {
+        testFlight = Generator.generateFlights("CPH", "DEN", 1).flights.get(0);
+        facade.addFlight(testFlight);
+        //Verify that user was actually inserted in the database
+        Flight flight = facade.getFlightByFlightId(testFlight.getFlightID());
+
+        assertEquals(testFlight.getFlightID(), flight.getFlightID());
+        assertEquals(testFlight.getFlightNumber(), flight.getFlightNumber());
+    }
+
 }
