aac84ac4c0f7520e52071d75a79958c4d6813920
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-05-18 10:35:44 +0200

    input validation added to flights rescource

--- src/main/java/rest/Flights.java
+++ src/main/java/rest/Flights.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import entity.Airline;
 import entity.Flight;
 import facades.FlightFacade;
+import httpErrors.AirlineException;
+import static java.lang.Integer.parseInt;
 import utils.Generator;
 
 import javax.persistence.EntityManagerFactory;
@@ -24,30 +26,55 @@ import java.util.List;
  */
 @Path("flights")
 public class Flights {
-
+    
     private EntityManagerFactory emf;
     private FlightFacade facade;
-
+    
     private static Gson gson = new Gson();
-
+    
     @Context
     private UriInfo context;
-
+    
     public Flights() {
         emf = Persistence.createEntityManagerFactory("pu_development");
         facade = new FlightFacade(emf);
     }
+    
+    private void inputValidation(String from, String date, int tickets) throws AirlineException {
+        inputValidation(from, "XXX", date, tickets);
+    }
 
+    private void inputValidation(String from, String to, String date, int tickets) throws AirlineException {
+        if (from.equals(to)) {
+            throw new AirlineException(3, 400, "You can't fly to the same place you are starting at!: " + from + "=" + to);
+        }
+        if (date.length() != 24) {
+            throw new AirlineException(3, 400, "You goofed your date formatting! Please follow the ISO standard: " + date);
+        }
+        if (from.length() != 3) {
+            throw new AirlineException(3, 400, "You goofed your starting airport! IATA codes consist of 3 capital letters!: " + from);
+        }
+        if (!to.equals("XXX") && to.length() != 3) {
+            throw new AirlineException(3, 400, "You goofed your destination airport! IATA codes consist of 3 capital letters!: " + to);
+        }
+        if (tickets <=0 || tickets >=300) {
+            throw new AirlineException(3, 400, "You goofed your tickets!  Should be between 1 and 300: " + tickets);
+        }
+        
+    }
+    
     @GET
     @Path("{from}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFlightsFromDateTickets(@PathParam("from") String from,
-                                            @PathParam("date") String date,
-                                            @PathParam("tickets") int tickets) {
+            @PathParam("date") String date,
+            @PathParam("tickets") int tickets) throws AirlineException {
+        inputValidation(from, date, tickets);
         List<Flight> flights = facade.getFlightsByOriginAndDate(from, date);
         if (flights != null && flights.size() > 0) {
-            for (int i = 0; i < flights.size(); i++)
+            for (int i = 0; i < flights.size(); i++) {
                 flights.get(i).setNumberOfSeats(tickets);
+            }
             Airline airline = new Airline();
             airline.airline = "JENS air";
             airline.flights = flights;
@@ -59,34 +86,37 @@ public class Flights {
             }
             return gson.toJson(airline);
         }
+        
     }
-
+    
     @GET
     @Path("{from}/{to}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFlightsFromDateTickets(@PathParam("from") String from,
-                                            @PathParam("to") String to,
-                                            @PathParam("date") String date,
-                                            @PathParam("tickets") int tickets) {
-        List<Flight> flights = facade.getFlightsByOriginAndDestinationAndDate
-                (from, to, date);
+            @PathParam("to") String to,
+            @PathParam("date") String date,
+            @PathParam("tickets") int tickets) throws AirlineException {
+        inputValidation(from, to, date, tickets);
+        List<Flight> flights = facade.getFlightsByOriginAndDestinationAndDate(from, to, date);
         if (flights != null && flights.size() > 0) {
-            for (int i = 0; i < flights.size(); i++)
+            for (int i = 0; i < flights.size(); i++) {
                 flights.get(i).setNumberOfSeats(tickets);
+            }
             Airline airline = new Airline();
             airline.airline = "JENS air";
             airline.flights = flights;
             return gson.toJson(airline);
         } else {
             Airline airline = Generator.generateFlights(from,
-                                                        to,
-                                                        date,
-                                                        tickets);
+                    to,
+                    date,
+                    tickets);
             for (int i = 0; i < airline.flights.size(); i++) {
                 facade.addFlight(airline.flights.get(i));
             }
             return gson.toJson(airline);
         }
+        
     }
-
+    
 }
